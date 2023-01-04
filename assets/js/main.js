8407db954562c2a5fc99a055d244fed0c3a0e0b4
vanngrann <ivanngrand@gmail.com>
2023-01-04 14:04:09 -0600

    form adjustment

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,6 +60,26 @@
   window.addEventListener('load', navbarlinksActive)
   onscroll(document, navbarlinksActive)
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+    
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
+  
+  document
+    .querySelector("form")
+    .addEventListener("submit", handleSubmit);
+  
+
   /**
    * Scrolls to an element with header offset
    */
